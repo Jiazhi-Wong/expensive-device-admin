f61d4896e649c3c536b8e273920e268d53e6addf
huangjiazhi <jiazhiwong8@gmail.com>
2017-04-09 00:40:23 +0800

    First commit

---
+++ src/pages/404.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="notFound-wrapper">
+    <p class="not-found">404</p>
+    <p class="not-found-text">该页面不存在</p>
+  </div>
+</template>
+
+<style scoped>
+  .not-found, .not-found-text {
+    font-size: 24px;
+    text-align: center;
+    color: rgb(192, 204, 218);
+  }
+
+  .not-found {
+    font-size: 72px;
+    margin-top: 240px;
+    margin-bottom: 30px;
+  }
+</style>
